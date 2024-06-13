42b7c801d418c043d7dec46bf15ec287a1c917f7
ngoxuanchien <74176040+ngoxuanchien@users.noreply.github.com>
2024-06-14 04:22:46 +0700

    add model

--- data-service/src/main/java/hcmus/zingmp3/dataservice/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package hcmus.zingmp3.dataservice.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- data-service/src/main/resources/db/migration/V1__init.sql
+++
@@ -1,6 +0,0 @@
-create table sentiment_data (
-    id varchar(36) primary key unique,
-    review longtext,
-    sentiment varchar(20),
-    language varchar(20)
-)
